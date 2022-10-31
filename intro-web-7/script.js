d0896e5f1dcccf24b262cbb4d90168cb40a82a7b
alansv96 <alansv96@gmail.com>
2022-10-31 17:31:51 -0600

    se agrego la parte de funciones de java script

---
+++ intro-web-7/script.js
@@ -0,0 +1,38 @@
+/* hola este
+es un comentario  */
+
+/* console.log("Hola mundo");
+alert("Alerta de chisme");
+console.log("esta es una suma", 5+5);
+ */
+var miNombre="Alan Sandoval"
+var miEdad=26
+var tengoHermanos=false
+var estadoAnimico = null
+var situacionFinanciera = undefined
+
+console.log(typeof(miNombre));
+console.log(typeof(miEdad));
+console.log(typeof(tengoHermanos));
+console.log(typeof(estadoAnimico));
+console.log(typeof(credito));
+
+console.log(Boolean(miNombre));
+
+var anioNacimiento = 1996
+var anioNacimientoDavid=1993
+var anioNacimientoAndi=1998
+var anioNacimientoAna=1998
+var anioNacimientoOli=1992
+var anioNacimientoMax=1999
+var anioActual= 2022
+console.log("Mi edad es: ",anioActual-anioNacimiento);
+console.log("Edad de david es: ",anioActual-anioNacimientoDavid);
+console.log("La edad de andi es: ",anioActual-anioNacimientoAndi);
+console.log("La edad de ana es: ",anioActual-anioNacimientoAna);
+console.log("La edad de Oli es : ",anioActual-anioNacimientoOli);
+console.log("La edad de Montiel es : ",anioActual-anioNacimientoMax);
+
+console.log(Number(undefined));
+console.log(String(undefined));
+console.log(Boolean(undefined));
